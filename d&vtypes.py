26f767f1375e19dfbee3fa9baf573432114354c8
D.Antoine <100112406+dantoine1991@users.noreply.github.com>
2022-02-27 10:04:01 -0500

    Add files via upload
    This is a python script showing the different data types and variables

---
+++ d&vtypes.py
@@ -0,0 +1,49 @@
+#Varible and Data types
+
+#Variable assigned to a string
+Pip = "cat"
+
+#Variable assigned to an interger (whole number)
+Age = 9
+
+#Variable assigned to a Boolean
+Chill = True
+
+#Variable assigned to another variable
+Variable2 = "Hi"
+Variable1 = Variable2
+
+#Variable assigned to different data types (List, Tuple, Dictionary, and Set)
+food = ("Pizza", "Chinese", "Steak", "Soup") 
+hero = {1 : "Goku", 2 : "Naruto", 3 : "Luffy"}
+villians = {"Frieza", "Madara", "The Navy"}
+videoGames = ["Resident Evil", "Super Mario", "Zelda"]
+
+
+#Here i am going to print the variables i created
+#Also, I am going to nest the "type" built-in python function to print the type of 
+#data type it is 
+print()
+print("Variable Types")
+print()
+print(Pip)
+print(Age)
+print(Chill)
+print(Variable1)
+print()
+print("Data Types")
+print()
+print(food)
+print(hero)
+print(villians)
+print(videoGames)
+print()
+print()
+print(type(Pip))
+print(type(Age))
+print(type(Chill))
+print(type(Variable1))
+print(type(food))
+print(type(hero))
+print(type(villians))
+print(type(videoGames))

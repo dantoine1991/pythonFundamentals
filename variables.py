6e61003e97fb1ef1a1b79242f5fe26595e2b1087
david <davidantoine1991@gmail.com>
2022-02-28 05:58:35 -0500

    This commit is dedicated to delete the files named 'newfile' for testing and add a new file called 'variables' to the remote repository

---
+++ variables.py
@@ -0,0 +1,14 @@
+greeting = "Sak Pase!"
+a, b, c = 1, 2, 3
+print(greeting * a)
+print(greeting * b)
+print(greeting * c)
+
+print(a + b + c)
+print(a * b * c)
+print(a**b)
+print(a**c)
+print(c - b - a)
+print(c - a)
+
+

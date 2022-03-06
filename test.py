8ab9d4b2857f021f12b10bb24d76ced334c5c641
David <davidantoine1991@gmail.com>
2022-03-05 23:15:42 -0500

    This is me testing out creating a new branch to work from

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 #This is a test script to see if the local repo from my linux PC can communicate to my remote repo (github/pythonFundamentals)
 #simple hello world script
 t = "Hello World"
+e = "I am David!"
+n = "and I like Python!"
 print(t)
+print(e)
+print(n)
+
+#or i couldve done this
+
+print("Hello World\n I am David\n and I like Python")
